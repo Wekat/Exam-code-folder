98be08211739e84b7b531aa7ec93b407f197ab55
Wekat <60980395+Wekat@users.noreply.github.com>
2020-05-19 11:05:03 +0200

    Done

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,41 @@
+// Smooth scroll using jquery
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+// On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+       });
+      }
+    }
+});
+
 
 //First slider
 $(document).ready(function(){
@@ -254,7 +292,7 @@ function initMap() {
     });
 }
 
-// submitting the form
+//submitting the form by using built in submit button rules by having "required" in the html
 
 let submitBtn = $('.contact__form--btn');
 let name = $('#name').val;
